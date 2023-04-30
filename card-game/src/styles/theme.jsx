ac66ed70540cddc2ba5a3f4cd9c99fb29b48b898
lydiacho <sheecho0804@ewhain.net>
2023-05-01 00:09:14 +0900

    feat : ResetBtn UI 구현

--- card-game/src/styles/theme.jsx
+++ card-game/src/styles/theme.jsx
@@ -2,6 +2,7 @@ const colors = {
     white: "#FFFFFF",
     black: "#000000",
     skyBlue: "#C7F5FE",
+    shadowBlue: "#57adbe",
     lightPink: "#FCC8F8",
     lightPurple: "#EAB4F8",
     purple: "#9900F0",
