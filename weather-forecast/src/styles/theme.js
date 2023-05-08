d0b4be9743707fd1d8b8653adef969380a873cb4
lydiacho <sheecho0804@ewhain.net>
2023-05-09 01:10:40 +0900

    init : styles 폴더에 theme, globalStyle 추가

---
+++ weather-forecast/src/styles/theme.js
@@ -0,0 +1,15 @@
+const colors = {
+  Sopt_Purple: "#ADA2FF",
+  Sopt_Blue: "#C0DEFF",
+  Sopt_Pink: "#FFE5F1",
+  Sopt_Light_Yellow: "#FFF8E1",
+  Sopt_Coral: "#FF8787",
+  Sopt_White: "#FFFFFF",
+  Sopt_Black: "#000000",
+};
+
+
+const theme = {
+  colors,
+};
+export default theme;
