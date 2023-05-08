b70a0555e7a3c7e09b5737a69c9a4097423b8921
lydiacho <sheecho0804@ewhain.net>
2023-05-08 21:31:58 +0900

    App에서 findMokey import 오탈자 문제 수정

--- card-game/src/App.jsx
+++ card-game/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { useState } from 'react';
 import { ThemeProvider } from "styled-components";
-import FindMonkey from './pages/FindMonkey';
+import FindMonkey from './pages/findMonkey';
 import ResetButton from './components/ResetButton';
 import GlobalStyle from "./styles/globalStyle";
 import theme from "./styles/theme";
