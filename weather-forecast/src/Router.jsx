983e4b734e3b2bb29105947493c5f189d38533a8
lydiacho <sheecho0804@ewhain.net>
2023-05-09 01:21:36 +0900

    init : Router 설정, pages 생성

--- weather-forecast/src/Router.jsx
+++ weather-forecast/src/Router.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import DayForecast from './pages/DayForecast';
+import WeekForecast from './pages/WeekForecast';
+
 
 
 const Router = () => {
   return (
     <BrowserRouter>
         <Routes>
-            <Route/>
+            <Route path="/" element={<Home/>} />
+            <Route path="/day" element={<DayForecast/>}>
+                <Outlet/>
+            </Route>
+            <Route path="/day" element={<WeekForecast/>}>
+                <Outlet/>
+            </Route>
         </Routes>
     </BrowserRouter>
   )
