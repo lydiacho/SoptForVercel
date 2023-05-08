0f3f496683803a44108345e964cc759c85878967
lydiacho <sheecho0804@ewhain.net>
2023-05-09 01:16:36 +0900

    init : App.jsx에 Router 추가

--- weather-forecast/src/App.jsx
+++ weather-forecast/src/App.jsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/globalStyle";
 import theme from "./styles/theme";
+import Router from "./Router";
 
 const App = () => (
   <>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Router/>
     </ThemeProvider>
   </>
 );
