94926aad9dc895bdd8ee71d759d2b10ef7e1c898
lydiacho <sheecho0804@ewhain.net>
2023-05-10 03:23:05 +0900

    feat : WeekCard API 기능 구현

--- weather-forecast/src/components/DayCard.jsx
+++ weather-forecast/src/components/DayCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useEffect, useState } from "react"
-
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import axios from 'axios';
--- weather-forecast/src/components/WeekCard.jsx
+++ weather-forecast/src/components/WeekCard.jsx
@@ -1,32 +1,61 @@
 import React from 'react'
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
-import styled from 'styled-components'
+import styled from "styled-components";
+import axios from 'axios';
+import { WEATER_TYPE } from '../constants/weather';
 
 const WeekCard = () => {
 
   const {area} = useParams();
+  const [dataList,setDataList] = useState([]);
+
+  const getWeekWeather = () => {
+    try {
+      axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${area}&appid=${import.meta.env.VITE_APP_WEATHER}&units=metric`)
+      .then(response => {
+        setDataList(response.data.list.filter((_,idx)=>idx%8===0));
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getWeekWeather();
+  }, [])
+
+  useEffect(() => {
+    getWeekWeather();
+  }, [area])
+
 
   return (
-      <St.CardWrapper>
-        <h1>05/17 {area}</h1>
-        <img src="https://i.pinimg.com/236x/fa/80/5a/fa805af9a1642835c51cdff085ab40f4.jpg"/>
-        <St.CardText>
-          <p>온도</p>
-          <p>11.75</p>
-        </St.CardText>
-        <St.CardText>
-          <p>체감 온도</p>
-          <p>10.91</p>
-        </St.CardText>
-        <St.CardText>
-          <p>최저/최고</p>
-          <p>00/00</p>
-        </St.CardText>
-        <St.CardText>
-          <p>구름</p>
-          <p>20%</p>
-        </St.CardText>
-      </St.CardWrapper>
+    <>
+      { dataList && 
+        dataList.map(({ dt_txt, weather, main, clouds }) => (
+          <St.CardWrapper key={dt_txt}>
+            <h1>{dt_txt}</h1>
+            <img src={WEATER_TYPE.filter(item => weather && (item.description === weather[0].description))[0].imgURL}/>
+            <St.CardText>
+              <p>온도</p>
+              <p>{main.temp}</p>
+            </St.CardText>
+            <St.CardText>
+              <p>체감 온도</p>
+              <p>{main.feels_like}</p>
+            </St.CardText>
+            <St.CardText>
+              <p>최저/최고</p>
+              <p>{main.temp_min}/{main.temp_max}</p>
+            </St.CardText>
+            <St.CardText>
+              <p>구름</p>
+              <p>{clouds.all}%</p>
+            </St.CardText>
+        </St.CardWrapper>))
+      }
+    </>
   )
 }
 
