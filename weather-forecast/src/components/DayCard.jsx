be08233b29bcfb7e56567af1eed524030684fe6a
lydiacho <sheecho0804@ewhain.net>
2023-05-10 02:29:46 +0900

    feat: DayCard API 기능 모두 구현

--- weather-forecast/src/components/DayCard.jsx
+++ weather-forecast/src/components/DayCard.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import axios from 'axios';
+import { WEATER_TYPE } from '../constants/weather';
 
 
 const DayCard = () => {
@@ -11,36 +12,54 @@ const DayCard = () => {
   const {area} = useParams();
   const [data,setData] = useState({});
 
-  useEffect(() => {
+  const getWeather = () => {
+    try {
       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${area}&appid=${import.meta.env.VITE_APP_WEATHER}&units=metric`)
       .then(response => {
         setData(response.data);
       })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getWeather();
   }, [])
+
+  useEffect(() => {
+    getWeather();
+  }, [area])
 
   const { name, weather, main, clouds } = data;
 
+  const imgUrl = WEATER_TYPE.filter(item => weather && (item.description === weather[0].description))
+
   return (
-    <St.CardWrapper>
-      <h1>{name}</h1>
-      <img src="https://i.pinimg.com/236x/fa/80/5a/fa805af9a1642835c51cdff085ab40f4.jpg"/>
-      <St.CardText>
-        <p>온도</p>
-        <p>{main.temp}</p>
-      </St.CardText>
-      <St.CardText>
-        <p>체감 온도</p>
-        <p>{main.feels_like}</p>
-      </St.CardText>
-      <St.CardText>
-        <p>최저/최고</p>
-        <p>{main.temp_min}/{main.temp_max}</p>
-      </St.CardText>
-      <St.CardText>
-        <p>구름</p>
-        <p>{clouds.all}%</p>
-      </St.CardText>
-    </St.CardWrapper>
+    <>
+      { weather && main && clouds &&
+        <St.CardWrapper>
+          <h1>{name}</h1>
+          {imgUrl && <img src={imgUrl[0].imgURL}/> }
+          <St.CardText>
+            <p>온도</p>
+            <p>{main?.temp}</p>
+          </St.CardText>
+          <St.CardText>
+            <p>체감 온도</p>
+            <p>{main?.feels_like}</p>
+          </St.CardText>
+          <St.CardText>
+            <p>최저/최고</p>
+            <p>{main?.temp_min}/{main?.temp_max}</p>
+          </St.CardText>
+          <St.CardText>
+            <p>구름</p>
+            <p>{clouds?.all}%</p>
+          </St.CardText>
+        </St.CardWrapper>
+      }
+    </>
   )
 }
 
