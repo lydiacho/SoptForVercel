f01f25b2fe324e55aa07bc3d15457f75c122c900
lydiacho <sheecho0804@ewhain.net>
2023-06-03 23:11:41 +0900

    fix : reset js->ts

--- card-game-ts/src/utils/reset.ts
+++ card-game-ts/src/utils/reset.ts
@@ -1,5 +1,4 @@
-
-const resetAll = (flippedList) => {
+const resetAll = (flippedList : HTMLElement[]) => {
     // reset 버튼 클릭 시 카드 초기화
     for (const item of flippedList) {
         item.classList.remove('flipped');
